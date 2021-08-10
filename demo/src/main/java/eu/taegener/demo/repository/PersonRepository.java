4bbd0d7784dfc6d45b1bdcc0888ab94e1a369df3
Eike Taegener <39055879+Eik0fresh@users.noreply.github.com>
2021-08-10 14:57:14 +0200

    Update PersonRepository.java
    Add Example Query

--- demo/src/main/java/eu/taegener/demo/repository/PersonRepository.java
+++ demo/src/main/java/eu/taegener/demo/repository/PersonRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+  
+  @Query(value="SELECT firstname FROM Person p WHERE p.lastname LIKE", nativeQuery=true)
+  Person getPersonBySurname(String surname);
+  
 }
