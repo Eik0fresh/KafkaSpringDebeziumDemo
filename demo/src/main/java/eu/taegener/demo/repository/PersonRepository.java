391b8d4df9c288f43f9dbb60ca3de2deb0d44cd0
Eike Taegener <eike.taegener@fu-berlin.de>
2022-05-30 12:32:26 +0200

    Gateway added. Issues fixed.

--- demo/src/main/java/eu/taegener/demo/repository/PersonRepository.java
+++ demo/src/main/java/eu/taegener/demo/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package eu.taegener.demo.repository;
 
 import eu.taegener.demo.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
